4ea89c5ec34b780d5a2e6937f26539fd3f96c5ce
mmcgraw73 <shout@mcgraw.io>
2022-12-10 20:29:46 -0500

    1 file modified

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -40,26 +40,6 @@ export default defineComponent({
     <h5>HelloWorld Component Starts Here</h5>
     <h3 style="color: red">{{ `GRANDCHILD BASE ID: ${fakeID}` }}</h3>
     <h3 style="color: green">{{ `CHILD DATA ARR: ${dumbdata}` }}</h3>
-    <p>
-      Recommended IDE setup:
-      <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-      +
-      <a href="https://marketplace.visualstudio.com/items?itemName=octref.vetur" target="_blank"> Vetur </a>
-    </p>
-
-    <p>See <code>README.md</code> for more information.</p>
-
-    <p>
-      <a href="https://vitejs.dev/guide/features.html" target="_blank"> Vite Docs </a>
-      |
-      <a href="https://vuejs.org/" target="_blank">Vue 2 Docs</a>
-    </p>
-
-    <button type="button" @click="count++">count is: {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test hot module replacement.
-    </p>
   </div>
 </template>
 
