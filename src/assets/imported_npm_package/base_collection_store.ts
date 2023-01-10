2918af212179c857dedab3a7ecb1ecabbcc1ebbb
mmcgraw73 <shout@mcgraw.io>
2023-01-09 23:04:14 -0500

    2 files modified

--- src/assets/imported_npm_package/base_collection_store.ts
+++ src/assets/imported_npm_package/base_collection_store.ts
@@ -22,15 +22,22 @@ export default class {
       dumbdata: [],
       id: '',
       page: 1,
+      name: 'BASE COLLECTION STORE',
     }),
       (this.getters = {
         dumbdata: (state) => state.dumbdata,
         id: (state) => state.id,
+        name: (state) => state.name,
+        placeholder: (state) => state.placeholder,
       }),
       (this.actions = {
-        get: ({ commit }, data) => {
+        get: ({ commit }, data, state) => {
           fetch('https://jsonplaceholder.typicode.com/posts/10')
-            .then((response) => response.json())
+            .then((response) => {
+              state.placeholder = response.json
+              console.log('state.placeholder', state.placeholder)
+              response.json()
+            })
             .then((json) => {
               console.log(json)
               commit(GET_BASE_COLLECTION_PENDING, json)
--- src/store/modules/base.js
+++ src/store/modules/base.js
@@ -1,10 +1,14 @@
 // import pseudo private package
+// this file would be in npm modules directory
+// this represents our api npm package that handles get requests 
+// as well as token authorization with api
 import BaseCollectionModule from '@/assets/imported_npm_package/base_collection_store.ts'
 
 // inside vue app we create...
 // this is all common/shared/required properties for every store
 export default class BaseStoreModule extends BaseCollectionModule {
   constructor() {
+    // js class requirement for allowing 
     super()
     this.namespaced = true
     console.log('context', this)
